408c14eb47b5da0cb991497d3451b8b5c77d8dce
Abhishek <ab@3computing.com>
2024-03-07 02:25:32 +0800

    router-controller setup

---
+++ src/App/Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+class HomeController
+{
+
+    public function index()
+    {
+        echo 'Hello from HomeController@index';
+    }
+}
--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -4,22 +4,46 @@ declare(strict_types=1);
 
 namespace Framework;
 
-class Router {
+class Router
+{
 
     private array $routes = [];
 
-    public function add(string $method, string $path){
+    public function add(string $method, string $path, array $controller)
+    {
         $path = $this->normalizePath($path);
         $this->routes[] = [
             'path' => $path,
-            'method' => strtoupper($method)
+            'method' => strtoupper($method),
+            'controller' => $controller
         ];
     }
 
-    private function normalizePath(string $path): string {
+    private function normalizePath(string $path): string
+    {
         $path = trim($path, '/');
         $path = "/{$path}/";
         $path = preg_replace('#[/]{2,}#', '/', $path);
         return $path;
+    }
+
+    public function dispatch(string $path, string $method)
+    {
+        $path = $this->normalizePath($path);
+        $method = strtoupper($method);
+
+        foreach ($this->routes as $route) {
+
+            // route is not a match
+            if (!preg_match("#^{$route['path']}$#", $path) || $route['method'] !== $method) {
+                continue;
+            }
+
+            // route matches
+            [$class, $function] = $route['controller'];
+
+            $controllerInstance = new $class;
+            $controllerInstance->$function();
+        }
     }
 }
